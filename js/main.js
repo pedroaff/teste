8be2fe5ce08554e66429acb55147b93f5798abe5
slainr <slain.sviddneger@gmail.com>
2018-04-03 02:00:29 -0300

    Refatorado o código. Criado o método para adicionar, editar e remover paciente.

--- js/main.js
+++ js/main.js
@@ -1,114 +1,198 @@
-$(function () {
-    $(document).ready(function(){
+$(document).ready(function () {
         $('.date').mask('0000/00/00');
-        $('.time').mask('00:00:00');
-        $('.date_time').mask('00/00/0000 00:00:00');
-        $('.cep').mask('00000-000');
         $('.phone').mask('00000-0000');
-        $('.phone_with_ddd').mask('(00) 0000-0000');
-        $('.phone_us').mask('(000) 000-0000');
-        $('.mixed').mask('AAA 000-S0S');
-        $('.cpf').mask('000.000.000-00', {reverse: true});
-        $('.money').mask('000.000.000.000.000,00', {reverse: true});
-    });      
-    
+        $('.cpf').mask('000.000.000-00', { reverse: true });
+        
+      
+        carregar();
+
+        SNButton.init("cadastrarPaciente", {
+			fields: ["nome", "celular", "cpf", "dataNascimento"]
+        });
+    });
+
+
     var $pacientes = $('#pacientes');
-    var cc = 0;
     var $nome = $('#nome');
     var $cpf = $('#cpf');
     var $dataNascimento = $('#dataNascimento');
-    var $id = $('#codigo');
     var $celular = $('#celular');
     var key = "23be778f-d904-4491-861a-6d226da05b49";
-    var urlbase = "http://si.shx.com.br:9999/shx-teste-backend/paciente/v2/";	
+    var urlbase = "http://si.shx.com.br:9999/shx-teste-backend/paciente/v2/";
+    var $idEdicao = 0;
 
-    //carrega a lista de pacientes
-    $(document).ready(function(){
+
+    var carregar = function () {
         $.ajax({
             type: 'GET',
             dataType: 'json',
-            url: urlbase + "/findall/" + key,
+            url: urlbase + "findall/" + key,
             success: function carregarDados(data) {
                 $pacientes.empty();
                 JSON.stringify(data);
-                //console.log(data.body[0].nome);        
-                $.each(data, function(i, paciente)
-                {       
+                $.each(data.body, function (i, paciente) {
                     $pacientes.append
                         (`
-                            <tr data-id=${data.body[cc].id}>
-                                <td> ${data.body[cc].nome}</td>
-                                <td> ${data.body[cc].cpf} </td>
-                                <td> ${data.body[cc].dataNascimento}  </td>  
-                                <td> ${data.body[cc].celular}  </td> 
+                            <tr data-id=${paciente.id}>
+                                <td> ${paciente.nome}</td>
+                                <td> ${paciente.cpf} </td>
+                                <td> ${paciente.dataNascimento}  </td>  
+                                <td> ${paciente.celular}  </td> 
                                 <td> 
-                                    <button type="button" class="btn btn-primary" id="editar" data-toggle="modal" data-target="#editarModal">Editar</button> 
-                                    <button type="button" class="btn btn-warning remover" data-id='${data.body[cc].id}'>Remover</button> 
+                                    <button type="button" class="btn btn-primary editar" id='${paciente.id}'>Editar</button> 
+                                    <button type="button" class="btn btn-warning remover" id='${paciente.id}')">Remover</button> 
+                                    
                                 </td>
                             </tr>`
                         );
-                        cc++;
+                });
+
+                $('.remover').on('click', function (e) {
+                    deletar(e.target.id);
+                });
+
+                $('.editar').on('click', function (e) {
+                    editar(e.target.id);
                 });
 
             },
-            error: function()
-            {
+            error: function () {
                 alert('Erro Ao tentar abrir o API');
                 document.write('<p>Erro ao tentar API.</p>');
             }
         });
-    });
+    }
 
-   
-            
-    $('#cadastrarPaciente').on('click', function() {
-        var novoPaciente = {
-            nome: $nome.val(),
-            dataNascimento: $dataNascimento.val(),
-            cpf: $cpf.val(),
-            celular: $celular.val()
+
+    var deletar = function (id) {
+        var data = {
+            id: id,
+            chave: key
         };
+
+        $.ajax({
+            type: 'DELETE',
+            contentType: "application/json",
+            dataType: "json",
+            url: urlbase + "remove",
+            data: JSON.stringify(data),
+            success: function (response) {
+                carregar();
+            },
+            error: function (error) {
+                alert(error)
+            }
+        });
+    }
+
+
+    var editar = function (id) {
+        var data = {
+            id: id,
+            chave: key
+        }
 
         $.ajax({
             type: 'POST',
             contentType: "application/json",
             dataType: "json",
-            url: urlbase + "persist/" + key,
-            data: JSON.stringify(novoPaciente),
-            success: function(criarPaciente) {
-                $pacientes.append(`
-                        <tr>
-                            <td>${novoPaciente.body.nome}</td>
-                            <td>${novoPaciente.body.dataNascimento}</td>
-                            <td>${novoPaciente.body.cpf}</td>
-                            <td>${novoPaciente.body.id}</td> 
-                            <td> 
-                                <button type="button" class="btn btn-info">Editar</button> 
-                                <button type="button" class="btn btn-warning remover">Remover</button> 
-                            </td>
-                        </tr>`);
-                $('#formularioModal').modal('hide');
-                $('form input:text').val('');                        
+            url: urlbase + "findone",
+            data: JSON.stringify(data),
+            success: function (response) {
+                mostrarDadosEdicao(response.body);
+            },
+            error: function (error) {
+                alert(error)
             }
         });
+    }
+
+    var habilitarBotao = function (){
+        $("#cadastrarPaciente").prop("disabled", "true");
+    }
+
+    var mostrarDadosEdicao = function (dados) {
+        $nome.val(dados.nome);
+        $dataNascimento.val(dados.dataNascimento);
+        $cpf.val(dados.cpf);
+        $celular.val(dados.celular);
+        $idEdicao = dados.id;
+
+        $('#labelCadastro').hide();
+        $('#cadastrarPaciente').hide();
+        $('#labelEdicao').show();
+        $('#editarPaciente').show();
+        $('#formularioModal').modal('show');
+    }
+
+
+    $('#showModalCadastrar').on('click', function () {
+        $('#adicionarPaciente')[0].reset();
+        $('#labelCadastro').show();
+        $('#cadastrarPaciente').show();
+        $('#labelEdicao').hide();
+        $('#editarPaciente').hide();
+
+        
+    });
+
+
+    $('#cadastrarPaciente').on('click', function () {
+        
+        var novoPaciente = {
+            nome: $nome.val(),
+            dataNascimento: $dataNascimento.val(),
+            cpf: $cpf.val().replace('.', '', ).replace('.', '').replace('-', ''),
+            celular: $celular.val().replace('-', ''),
+            chave: key
+        };            
+
+            $.ajax({
+                type: 'POST',
+                contentType: "application/json",
+                dataType: "json",
+                url: urlbase + "persist",
+                data: JSON.stringify(novoPaciente),
+                beforeSend: function(){
+                    
+                },
+                success: function (criarPaciente) {
+                    carregar();
+                    $('#formularioModal').modal('hide');
+                    $('form input:text').val('');               
+                }
+            });
+        
 
     });
 
-    //Remover paciente
-    // var $tabela = $('#pacienteTabela');
+    
+    $('#editarPaciente').on('click', function (e) {
 
-    // $tabela.delegate('.remover', 'click', function(){
-    //     var $li = $(this).closest('li');
-    //     $.ajax({
-    //         type: 'DELETE',
-    //         url: urlbase + $(this).attr('data-id'),
-    //         contentType: "application/json",
-    //         dataType: "json",
-    //         success: function (){
-    //             $li.remove();
-    //         }
-    //     });
-    // });
+        var pacienteEditado = {
+            nome: $nome.val(),
+            dataNascimento: $dataNascimento.val(),
+            cpf: $cpf.val().replace('.', '', ).replace('.', '').replace('-', ''),
+            celular: $celular.val().replace('-', ''),
+            chave: key,
+            id: $idEdicao
+        };        
 
+        $.ajax({
+            type: 'PUT',
+            contentType: "application/json",
+            dataType: "json",
+            url: urlbase + "merge",
+            data: JSON.stringify(pacienteEditado),
+            success: function (response) {
+                $('#formularioModal').modal('hide');
+                $('form input:text').val('');
+                carregar();
 
-});
+            }
+        })
+    });
+
+    $('#cancelar').on('click', function(e) {
+        $('form input:text').val('');
+    });
